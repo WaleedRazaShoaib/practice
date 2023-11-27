3ee8fd1eaad13f947bb444cd84aff92866f49126
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2023-11-27 12:57:16 +0500

    practice .js

---
+++ app.js
@@ -0,0 +1,167 @@
+//   CHAPTER NO 25 TO 30;  
+
+//  QUESTION NO O1;
+
+// var input = +prompt("write your number ");
+// console.log(input);
+// console.log(Math.round(input));
+// console.log(Math.floor(input));
+// console.log(Math.ceil(input));
+
+//  QUESTION NO 02;
+
+// var input = -prompt("write your number ");
+// console.log(input);
+// console.log(Math.round(input));
+// console.log(Math.floor(input));
+// console.log(Math.ceil(input));
+
+//  QUESTIION NO 03 ;
+
+// var num = -4;
+// console.log(num);
+// console.log(Math.round(-num) )
+
+//   QUESTION NO 04 ;
+
+// var headuser = +prompt("Enter heads user");
+// var tailsuser = +prompt("Enter tails user");
+// var toss = Math.random() * 2 ;
+// var floor = Math.floor(toss);
+// if ( floor === 0 ){
+//     alert("heads " + headuser + " win the toss")
+// }
+// else {
+//     alert("tails " + tailsuser + " win the toss")
+// }
+
+// QUESTION NO 05;
+
+// function generateRandomNumber() {
+// var min = 1;
+// var max = 100;
+// var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+// return randomNumber;
+// }
+// var randomNumberElement = document.getElementById("randomNumber");
+// randomNumberElement.textContent = generateRandomNumber();
+   
+//  QUSETION NO 06;
+
+
+
+
+//   QUESTION NO 07;
+
+// var numi = +prompt("Write the number 1 to 10");
+// var min1 = 1 ;
+// var max1 = 10 ;
+// var randomNumber1 = Math.floor(Math.random() * (max1 - min1 + 1)) + min1;
+
+// if ( randomNumber1 === numi){ 
+//     alert ("u Found the write number")
+// }
+// else{
+//     alert("U didn't the write number")
+// }
+
+
+//  CHAPTER NO 30 TO 35 ;
+
+
+//   QUESTION NO 1;
+
+var date = new Date ;
+// console.log(date);
+
+//   QUESTION NO 2;
+
+var d = new Date(); 
+//  var currentMonth = d.getMonth();
+// document.write( d + "   " + currentMonth);
+
+//    QUESTION NO 3 ;
+
+// var opo =  d.getDay();
+// console.log(opo)
+
+// if (opo === 6){
+//         alert("today is sat")
+// }
+// else{
+//         alert("Something went wrong")
+// }
+
+///    QUESTION NO 4 ;
+ 
+// var opo =  d.getDay();
+// console.log(opo)
+
+// if (opo === 6){
+//         alert("today is FUNDAY")
+// }
+// else{
+//         alert("Something went wrong")
+// }
+
+
+//    QUESTION  NO 5 ;
+
+// var ulo = d.getDate();
+// console.log(ulo);
+//  if (ulo < 15){
+//         alert ("First fifteen of the month")
+//  } else{
+//         alert("Something went wrong")
+//  }
+
+//   QUESTION NO 6 ;
+
+// var tu = d.getTime();
+// console.log(tu + " this is milli second ");
+// console.log(tu * 60 + " this is second ");
+
+///   QUESTION NO 07 ;
+
+var hours = d.getHours();
+console.log(hours);
+if( hours <= 12 ){
+        alert("its pm")
+}
+else{
+alert("Something went wrong")
+}
+
+//     QUESTION NO 08 ;
+
+
+// var laterDate = new Date(2020, 11, 31);
+// console.log(laterDate);
+
+//   QUESTION NO 09 ;
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
